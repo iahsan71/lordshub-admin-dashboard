bbd08fef640e56811896641dc025cf970869d8dc
hamidtalib <hamid_talib@nextpak.org>
2025-12-04 17:33:23 +0500

    handle name

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.gm32079jgmo"
+    "revision": "0.5ag9qsp7aa"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
